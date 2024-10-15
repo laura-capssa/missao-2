a6587e0e5a442c02969bd2147853a578de4a30fb
laura-capssa <laucapssa@gmail.com>
2024-10-14 22:24:48 -0300

    Adiciona o diretório myNewApp e configurações iniciais

---
+++ myNewApp/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myNewApp',
+  webDir: 'dist'
+};
+
+export default config;
